a73f57365b71ebc40845112835d4fc5484d11be2
Jaime <jaimetravesedo125@gmail.com>
2021-02-24 19:52:21 +0100

    Working package

--- perIOdico/perIOdico.py
+++ perIOdico/perIOdico.py
@@ -4,38 +4,40 @@ from datetime import datetime
 import requests
 import shutil
 
-import sys
-import subprocess
+import os
 
 
 def get_date():
 	while True:
 		try:
 			date_str = input("When were you born? (DD-MM-YYYY): ")
-			date = datetime.strptime(date_str, '%d-%m-%Y')
-			return date
+			return datetime.strptime(date_str, '%d-%m-%Y')
 		except ValueError:
 			print("Date is incorrect, try again")
 
 
 class perIOdico:
-	def __init__(date, verbose=False):
+	def __init__(self, date, verbose=False):
 		self.verbose = verbose
 		str_date = date.strftime('%Y/%m/%d')
-		self.url = 'https://elpais.com/hemeroteca/elpais/portadas/{}'.format(date_str) 
+		self.url = 'https://elpais.com/hemeroteca/elpais/portadas/{}'.format(str_date) 
 
-	def get_image():
-		self.response = requests.get(url)
+	def get_image(self):
+		try:
+			self.response = requests.get(self.url)
+		except requests.exceptions.MissingSchema:
+			if self.verbose: 
+				print('There is not a page for that day')
 
-		soup = BeautifulSoup(response.content, 'html.parser')
+		soup = BeautifulSoup(self.response.content, 'html.parser')
 		image_url = soup.find_all('img')[0].get('src')
 
 		self.image = requests.get(image_url, stream=True)
 
-		if image_response.status_code == 200:
+		if self.image.status_code == 200:
 			with open("img.jpg", 'wb') as f:
-				image_response.raw.decode_content = True
-				shutil.copyfileobj(image_response.raw, f)
+				self.image.raw.decode_content = True
+				shutil.copyfileobj(self.image.raw, f)
 		else:
 			if self.verbose:
 				print("I could not find first page for that day")
@@ -44,11 +46,4 @@ class perIOdico:
 		if self.verbose:
 			print("Extracted correctly")
 
-	if sys.platform.startswith('linux'):
-        ret_code = subprocess.call(['xdg-open', something_to_open])
-
-    elif sys.platform.startswith('darwin'):
-        ret_code = subprocess.call(['open', something_to_open])
-
-    elif sys.platform.startswith('win'):
-        ret_code = subprocess.call(['start', something_to_open], shell=True)
+		os.system('xdg-open ./img.jpg')
