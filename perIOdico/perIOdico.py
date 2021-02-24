4ee8379ae79cd0966f8bcd3ce96d1b4de6f0d115
Jaime <jaimetravesedo125@gmail.com>
2021-02-24 18:57:15 +0100

    NOW created class

--- perIOdico/perIOdico.py
+++ perIOdico/perIOdico.py
@@ -1,36 +1,39 @@
 #venv/bin/python3.8
+from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
-from bs4 import BeautifulSoup
 import shutil
 
-def main():
 
+def getDate():
 	while True:
 		try:
 			date_str = input("When were you born? (DD-MM-YYYY): ")
 			date = datetime.strptime(date_str, '%d-%m-%Y')
-			break
+			return date
 		except ValueError:
 			print("Date is incorrect, try again")
 
-	url = 'https://elpais.com/hemeroteca/elpais/portadas/{}'.format(date.strftime('%Y/%m/%d')) 
-	response = requests.get(url)
 
-	soup = BeautifulSoup(response.content, 'html.parser')
-	image_url = soup.find_all('img')[0].get('src')
+class perIOdico:
+	def __init__(date):
+		str_date = date.strftime('%Y/%m/%d')
+		self.url = 'https://elpais.com/hemeroteca/elpais/portadas/{}'.format(date_str) 
 
-	image_response = requests.get(image_url, stream=True)
+	def get_image():
+		self.response = requests.get(url)
 
-	if image_response.status_code == 200:
-		with open("first_page.jpg", 'wb') as f:
-			image_response.raw.decode_content = True
-			shutil.copyfileobj(image_response.raw, f)
-	else:
-		print("I could not find first page for you that day")
-		return
+		soup = BeautifulSoup(response.content, 'html.parser')
+		image_url = soup.find_all('img')[0].get('src')
 
-	print("Extracted correctly")
+		self.image = requests.get(image_url, stream=True)
 
-if __name__=="__main__":
-	main()
+		if image_response.status_code == 200:
+			with open("first_page.jpg", 'wb') as f:
+				image_response.raw.decode_content = True
+				shutil.copyfileobj(image_response.raw, f)
+		else:
+			print("I could not find first page for that day")
+			return
+
+		print("Extracted correctly")
